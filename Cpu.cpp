8424069a9a7785c6a2dcb1d57a6e95b1b9a62acc
Elliot Heisler <elliotheisler9@gmail.com>
2023-07-26 09:38:33 -0600

    filled out more specification of Cpu design

--- Cpu.cpp
+++ Cpu.cpp
@@ -3,16 +3,62 @@
 #include "Cpu.hpp"
 #include "common.hpp"
 
-Cpu::Cpu(Mapper new_mapper)
-      : mapper { new_mapper }
-    {}
+void Cpu::clock() {
+        counter++;
+        if (cycles() == counter) {
+            exec();
+            counter = 0;
+        }
+}
 
-uint8_t* Cpu::apply_addr(uint16_t addr) {
+void Cpu::do_poweron() {
+    // https://www.nesdev.org/wiki/CPU_power_up_state#At_power-up
+    SR = 0b00110100;
+    A = X = Y = 0x00;
+    SP = 0b11111101;
+    store8(0x4017, 0); // frame irq enabled
+    store8(0x4015, 0); // all channels disabled
+    for (uint16_t addr = 0x4000; addr <= 0x4005; addr++) {
+        store8(addr, 0);
+    }
+    for (uint16_t addr = 0x4010; addr <= 0x4013; addr++) {
+        store8(addr, 0);
+    }
+    // TODO:
+//     All 15 bits of noise channel LFSR = $0000[5]. The first time the LFSR is clocked from the all-0s state, it will shift in a 1.
+//     APU Frame Counter:
+// 
+//         2A03E, G, various clones: APU Frame Counter reset.
+//         2A03letterless: APU frame counter powers up at a value equivalent to 15
+// 
+//     Internal memory ($0000-$07FF) has unreliable startup state. Some machines may have consistent RAM contents at power-on, but others do not.
+// 
+//         Emulators often implement a consistent RAM startup state (e.g. all $00 or $FF, or a particular pattern), and flash carts like the PowerPak may partially or fully initialize RAM before starting a program, so an NES programmer must be careful not to rely on the startup contents of RAM.
+}
+
+void Cpu::do_reset() {
+    // https://www.nesdev.org/wiki/CPU_power_up_state#After_reset
+    using enum Cpu::CpuFlag;
+    SP -= 3;
+    set_flag(fInterruptDisable, true);
+    store8(0x4015, 0);
+    // TODO:
+//     APU triangle phase is reset to 0 (i.e. outputs a value of 15, the first step of its waveform)
+//     APU DPCM output ANDed with 1 (upper 6 bits cleared)
+// 
+//         APU Frame Counter:
+//         2A03E, G, various clones: APU Frame Counter reset.
+//         2A03letterless: APU frame counter retains old value [6]
+}
+
+uint8_t* Cpu::apply_addr8(uint16_t addr) {
     // https://www.nesdev.org/wiki/CPU_memory_map
     uint16_t actual_addr;
     if (check_bits("000x_xxxx_xxxx_xxxx", addr)) {
+        // 2KB internal ram
     }
     else if (check_bits("001x_xxxx_xxxx_xxxx", addr)) {
+        // 8 NES PPU registers
     } 
     else {
             if (addr >= 0b0100000000100000) {
--- Cpu.hpp
+++ Cpu.hpp
@@ -2,12 +2,51 @@
 #include "Mapper.hpp"
 class Cpu {
   public:
-    Mapper mapper;
+    enum class CpuFlag {
+        fCarry,
+        fZero,
+        fInterruptDisable,
+        fDecimal,
+        // https://www.nesdev.org/wiki/Status_flags#The_B_flag
+        fUnusedFlag1,
+        fUnusedFlag2,
+        fOverflow,
+        fNegative
+    };
+
+    Cpu(); // TODO: Cartridge parameter
+    // set cpu to power-on state
+    void do_poweron();
+    // perform state changes equivalent to hitting the NES reset button
+    void do_reset();
+    
+
+    void clock();
+
+  private:
+    uint8_t A, Y, X, SP, SR;
+    uint16_t PC;
+
+    // TODO: Cartridge cartridge;
+    // only the cpu communicates with the APU and the NES's 2kb of RAM
+    // TODO: Apu apu
     uint8_t ram[1 << 11];
 
-    Cpu(Mapper new_mapper);
-    
-    uint8_t* apply_addr(uint16_t addr);
+    int counter;
 
-     
+
+    // number of cycles that the next instruction will take
+    int cycles();
+    // execute current instruction
+    void exec();
+    // load/store functions
+    uint8_t load8(uint16_t addr);
+    uint16_t load16(uint16_t addr);
+    void store8(uint16_t addr, uint8_t data);
+    void store16(uint16_t addr, uint16_t data);
+    uint16_t* apply_addr16(uint16_t addr);
+    uint8_t* apply_addr8(uint16_t addr);
+    // flag functions
+    bool get_flag(CpuFlag flag);
+    void set_flag(CpuFlag flag, bool val);
 };
